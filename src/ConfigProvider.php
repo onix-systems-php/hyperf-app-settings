233e5a2d81325660ae5e4d47bc0d2b531570c81b
Vladimir Mikhav <vladimir.mikhav@onix-systems.com>
2022-12-27 08:13:02 +0200

    fix: update config provider

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 namespace OnixSystemsPHP\HyperfAppSettings;
 
-use Hyperf\Contract\ConfigInterface;
 use Hyperf\HttpServer\Router\Router;
 use Hyperf\Utils\ApplicationContext;
 
@@ -12,14 +11,9 @@ class ConfigProvider
     public function __invoke(): array
     {
         if (ApplicationContext::hasContainer()) {
-            /** @var ConfigInterface $container */
-            $config = ApplicationContext::getContainer()->get(ConfigInterface::class);
-            if (in_array('app-settings', $config->get('extensions', []))) {
-                Router::addGroup('/v1/admin/app_settings', function () {
-                    Router::get('', [Controller\AppSettingsController::class, 'index']);
-                    Router::post('', [Controller\AppSettingsController::class, 'update']);
-                });
-            }
+            Router::addGroup('/v1/admin/action_logs', function () {
+                Router::get('', [Controller\ActionLogsController::class, 'index']);
+            });
         }
 
         return [
