70dd9f6e4db6eee8eb52c6a8e902ff389a547192
Alex Syvolap <80973788+alexsyvolap@users.noreply.github.com>
2023-12-05 18:37:13 +0200

    Update file headers across the project

--- src/Repository/AppSettingsRepository.php
+++ src/Repository/AppSettingsRepository.php
@@ -2,19 +2,16 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.
+ * This file is part of the extension library for Hyperf.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
 namespace OnixSystemsPHP\HyperfAppSettings\Repository;
 
 use Hyperf\Cache\Annotation\Cacheable;
-use Hyperf\Contract\ConfigInterface;
 use Hyperf\Context\ApplicationContext;
+use Hyperf\Contract\ConfigInterface;
 use OnixSystemsPHP\HyperfAppSettings\Model\AppSetting;
 use OnixSystemsPHP\HyperfCore\Constants\Time;
 use OnixSystemsPHP\HyperfCore\Model\Builder;
